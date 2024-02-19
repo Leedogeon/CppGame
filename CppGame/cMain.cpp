3080f95dabc5943a6690f892cf523eac02844004
Leedogeon <jszs98@naver.com>
2024-02-19 19:50:07 +0900

    Update cMain.cpp
    충돌 미완

--- CppGame/cMain.cpp
+++ CppGame/cMain.cpp
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
-
+#define BulletCount 10
+#define EnemyCount 10
 #pragma region Enum
 enum COLOR
 {
@@ -32,8 +33,9 @@ struct Player
 	bool act;
 	int x;
 	int y;
+	int aniIndex;
 	COLOR color;
-	const char* shape[2];
+	const char* shape[10][3];
 };
 
 struct Enemy
@@ -45,13 +47,21 @@ struct Enemy
 	const char* shape[3];
 };
 
+struct Bullet
+{
+	bool act;
+	int x;
+	int y;
+	COLOR color;
+	const char* shape;
+};
 #pragma endregion
 
 
 #pragma region Game
 Player* player = nullptr;
-Enemy* enemy = nullptr;
-
+Enemy* enemy[EnemyCount] = {};
+Bullet* bullets[BulletCount] = {};
 void StageInitialize();
 void StageProgress();
 void StageRender();
@@ -76,7 +86,7 @@ int main()
 		system("cls");
 		StageProgress();
 		StageRender();
-		Sleep(50);
+		Sleep(20);
 
 	}
 
@@ -91,23 +101,86 @@ void StageInitialize()
 
 	player = (Player*)malloc(sizeof(Player));
 	player->act = true;
-	player->x = 5;
-	player->y = 5;
 	player->color = WHITE;
-	player->shape[0] = "＠＠";
-	player->shape[1] = "＠＠";
+	player->x = 10;
+	player->y = 10;
+	player->color = WHITE;
+	player->aniIndex = 0;
+	player->shape[0][0] = "   -----|-----";
+	player->shape[0][1] = "*>=====[_]L)";
+	player->shape[0][2] = "      -'-`-";
 
-	enemy = (Enemy*)malloc(sizeof(Enemy));
-	enemy->act = true;
-	enemy->x = 10;
-	enemy->y = 10;
-	enemy->color = YELLOW;
-	enemy->shape[0] = "□□□□";
-	enemy->shape[1] = "□□□□";
-	enemy->shape[2] = "□□□□";
+	player->shape[1][0] = "    ----|---- ";
+	player->shape[1][1] = "*>=====[_]L)";
+	player->shape[1][2] = "      -'-`-";
+
+	player->shape[2][0] = "     ---|---  ";
+	player->shape[2][1] = "*>=====[_]L)";
+	player->shape[2][2] = "      -'-`-";
+
+	player->shape[3][0] = "      --|--   ";
+	player->shape[3][1] = "*>=====[_]L)";
+	player->shape[3][2] = "      -'-`-";
+
+	player->shape[4][0] = "       -|-    ";
+	player->shape[4][1] = "*>=====[_]L)";
+	player->shape[4][2] = "      -'-`-";
+
+	player->shape[5][0] = "        +     ";
+	player->shape[5][1] = "*>=====[_]L)";
+	player->shape[5][2] = "      -'-`-";
+
+	player->shape[6][0] = "       -|-    ";
+	player->shape[6][1] = "*>=====[_]L)";
+	player->shape[6][2] = "      -'-`-";
+
+	player->shape[7][0] = "      --|--   ";
+	player->shape[7][1] = "*>=====[_]L)";
+	player->shape[7][2] = "      -'-`-";
+
+	player->shape[8][0] = "     ---|---  ";
+	player->shape[8][1] = "*>=====[_]L)";
+	player->shape[8][2] = "      -'-`-";
+
+	player->shape[9][0] = "    ----|---- ";
+	player->shape[9][1] = "*>=====[_]L)";
+	player->shape[9][2] = "      -'-`-";
+
+
+	for (int i = 0; i < BulletCount; i++)
+	{
+		bullets[i] = (Bullet*)malloc(sizeof(Bullet));
+		bullets[i]->act = false;
+		bullets[i]->x = i;
+		bullets[i]->y = 0;
+		bullets[i]->color = LIGHTBLUE;
+		bullets[i]->shape = "●";
+	}
+
+
+	for (int i = 0; i < EnemyCount; i++)
+	{
+		enemy[i] = (Enemy*)malloc(sizeof(Enemy));
+		enemy[i]->act = true;
+		enemy[i]->x = 20;
+		enemy[i]->y = rand()%20 + 10;
+		enemy[i]->color = YELLOW;
+		enemy[i]->shape[0] = "□□□□";
+		enemy[i]->shape[1] = "□□□□";
+		enemy[i]->shape[2] = "□□□□";
+	}
+
+	//enemy = (Enemy*)malloc(sizeof(Enemy));
+	//enemy->act = true;
+	//enemy->x = 20;
+	//enemy->y = 10;
+	//enemy->color = YELLOW;
+	//enemy->shape[0] = "□□□□";
+	//enemy->shape[1] = "□□□□";
+	//enemy->shape[2] = "□□□□";
 
 }
-
+int enemyspeed;
 void StageProgress()
 {
 	if (GetAsyncKeyState(VK_LEFT))
@@ -129,43 +202,143 @@ void StageProgress()
 		player->y++;
 	}
 
-	if ((enemy->x <= (player->x)|| (enemy->x <= (player->x+1))) && ((player->x < enemy->x + 4) || (player->x+1 < enemy->x + 4) ) &&
-		((enemy->y <= player->y)||(enemy->y <= player->y+1)) && ((player->y < enemy->y + 3)||(player->y+1 < enemy->y + 3)))
+	if (GetAsyncKeyState(VK_SPACE))
 	{
-		enemy->color = RED;
+		for (int i = 0; i < BulletCount; i++)
+		{
+			if (bullets[i]->act == false)
+			{
+
+				bullets[i]->x = player->x + 6;
+				bullets[i]->y = player->y + 1;
+				bullets[i]->act = true;
+				break;
+			}
+		}
 	}
-	else
+
+	for (int i = 0; i < BulletCount; i++)
 	{
-		enemy->color = YELLOW;
+		//if (bullets[i]->act)
+		if (bullets[i]->act == true)
+		{
+			bullets[i]->x++;
+
+			for (int j = 0; j < EnemyCount; j++)
+			{
+				if (bullets[i]->x >= enemy[j]->x && bullets[i]->x <= enemy[j]->x + 4 && bullets[i]->y >= enemy[j]->y && bullets[i]->y <= enemy[j]->y + 2)
+				{
+					enemy[j]->x = 40;
+				}
+			}
+
+			
+
+
+
+			if (bullets[i]->x >= 40)
+			{
+				bullets[i]->x = i;
+				bullets[i]->y = 0;
+				bullets[i]->act = false;
+			}
+		}
 	}
+	enemyspeed++;
+	/*if (enemyspeed == 2)
+	{
+		enemy->x--;
+		if (enemy->x == 0) enemy->x = 40;
+		enemyspeed = 0;
+	}*/
+	
+	for (int i = 0; i < EnemyCount; i++)
+	{
+		if ((enemy[i]->x <= (player->x) || (enemy[i]->x <= (player->x + 6))) && ((player->x < enemy[i]->x + 4) || (player->x + 6 < enemy[i]->x + 4)) &&
+			((enemy[i]->y <= player->y) || (enemy[i]->y <= player->y + 2)) && ((player->y < enemy[i]->y + 3) || (player->y + 2 < enemy[i]->y + 3)))
+		{
+			enemy[i]->color = RED;
+		}
+		else
+		{
+			enemy[i]->color = YELLOW;
+		}
+	}
+	
 }
 
+int stageCount = 0;
 
 void StageRender()
 {
 
+	stageCount++;
+	if (stageCount == 3)
+	{
+		player->aniIndex++;
+
+		player->aniIndex %= 10;
+		stageCount = 0;
+	}
 	for (int i = 0; i < 3; i++)
 	{
-		MovePos(enemy->x, enemy->y + i);
-		Paint(enemy->color);
-		printf(enemy->shape[i]);
-
-	}
-
-	for (int i = 0; i < 2; i++)
-	{
-		MovePos(player->x, player->y+i);
+		MovePos(player->x, player->y + i);
 		Paint(player->color);
-		printf(player->shape[i]);
+		printf(player->shape[player->aniIndex][i]);
 	}
-		
 
-	
-	
+
+	for (int i = 0; i < EnemyCount; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			MovePos(enemy[i]->x, enemy[i]->y + j);
+			Paint(enemy[i]->color);
+			printf(enemy[i]->shape[j]);
+		}
+
+	}
+
+
+	for (int i = 0; i < BulletCount; i++)
+	{
+		MovePos(bullets[i]->x, bullets[i]->y);
+		Paint(bullets[i]->color);
+		printf(bullets[i]->shape);
+	}
+
+
+
+
 }
 
 void StageRelease()
 {
+	if (player != nullptr)
+	{
+		free(player);
+		player = nullptr;
+	}
+
+	for (int i = 0; i < BulletCount; i++)
+	{
+		if (bullets[i] != nullptr)
+		{
+			free(bullets[i]);
+			bullets[i] = nullptr;
+		}
+	}
+
+	for (int i = 0; i < EnemyCount; i++)
+	{
+		if (enemy[i] != nullptr)
+		{
+			free(enemy[i]);
+			enemies[i] = nullptr;
+		}
+	}
+
+
 }
 #pragma endregion
 
